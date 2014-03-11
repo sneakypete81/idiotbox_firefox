c9e0680130ca45c002c101f753c8fb4b8190bc85
sneakypete81 <sneakypete81@gmail.com>
2014-03-11 21:47:40 +0000

    Updated IMDB link inserter to work with the new iPlayer site

--- data/imdb_content_script.js
+++ data/imdb_content_script.js
@@ -3,14 +3,20 @@
 var regexp = /^BBC iPlayer - (.*)/;
 var title = regexp.exec(document.title)[1];
 
-var link = document.createElement('a');
-link.href = 'http://www.imdb.com/find?q='+title;
-link.textContent = "IMDB";
+var abbr = document.createElement("abbr");
+abbr.setAttribute("class", "item");
+abbr.setAttribute("title", "IMDB");
+abbr.setAttribute("style", "width: 28px; left: 10px;");
+abbr.textContent = "IMDB";
 
-var list_item = document.createElement('li');
-list_item.setAttribute('style', 'margin-left: 30px;');
-list_item.appendChild(document.createTextNode(' '));
+var link = document.createElement("a");
+link.setAttribute("class", "link")
+link.href = "http://www.imdb.com/find?q=" + title;
+link.textContent = "Look up on IMDB";
+link.appendChild(abbr);
+
+var list_item = document.createElement("li");
 list_item.appendChild(link);
 
-var heading_list = document.getElementById('blq-local-nav');
-heading_list.appendChild(list_item);
+var heading_list = document.getElementById("external").getElementsByTagName("ul")[0];
+heading_list.insertBefore(list_item, heading_list.firstChild);
